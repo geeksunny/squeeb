5d72b1e21435661c3006d1a5f29b75669c25926f
Justin Swanson <geeksunny@gmail.com>
2021-02-14 01:46:41 -0600

    QueryBuilder updates

--- smrtypntz/squeeb/query.py
+++ smrtypntz/squeeb/query.py
@@ -1,11 +1,12 @@
 from __future__ import annotations
+
 import string
 from collections import namedtuple
 from enum import Enum
-from typing import Any, Dict, Union, Iterable, List
+from typing import Any, Dict, Iterable, List, Tuple, Union
 
 
-Query = namedtuple('Query', ['query', 'args', 'error'])
+Query = namedtuple('Query', ['query', 'args', 'error'], defaults=(None, ))
 
 
 class ConditionError(Exception):
@@ -36,20 +37,35 @@ class Junction(_StrEnum):
     OR = " OR "
 
 
+class _IStringable(object):
+
+    def __str__(self) -> str:
+        raise NotImplementedError()
+
+
 class _QueryValueHandlerMixin(object):
 
     def _get_values(self) -> Iterable[Any]:
         raise NotImplementedError()
 
     @property
-    def values(self) -> Iterable[Any]:
+    def value_args(self) -> Iterable[Any]:
         return self._get_values()
 
 
-class _ICondition(_QueryValueHandlerMixin):
+class _IQueryValueStrings(object):
 
-    def __str__(self) -> str:
+    @property
+    def column_str(self) -> str:
         raise NotImplementedError()
+
+    @property
+    def values_str(self) -> str:
+        raise NotImplementedError()
+
+
+class _ICondition(_IStringable, _QueryValueHandlerMixin):
+    pass
 
 
 class _IJuncture(object):
@@ -185,7 +201,7 @@ class _ConditionSequence(_ICondition):
         values = []
         for condition in self._conditions:
             if isinstance(condition, _ICondition):
-                values.extend(condition._get_values())
+                values.extend(condition.value_args)
         return values
 
     def __str__(self) -> str:
@@ -235,7 +251,7 @@ class ConditionGroup(list, _ICondition, _IJuncture):
     def _get_values(self) -> Iterable[Any]:
         values = []
         for condition in self._conditions:
-            values.extend(condition._get_values())
+            values.extend(condition.value_args)
         return values
 
     def __str__(self) -> str:
@@ -256,10 +272,63 @@ class ConditionGroup(list, _ICondition, _IJuncture):
         return self.__join(Junction.OR)
 
 
+class _QueryValueMap(dict, _QueryValueHandlerMixin, _IQueryValueStrings):
+
+    def _get_values(self) -> Iterable[Any]:
+        return self.values()
+
+    @property
+    def column_str(self) -> str:
+        return "(%s)" % ", ".join(self.keys())
+
+    @property
+    def values_str(self) -> str:
+        return "(%s)" % ", ".join("?" * len(self))
+
+    @property
+    def value_set_str(self) -> str:
+        return ", ".join(map(lambda key: "%s = ?" % key, self.keys()))
+
+
+class _QueryValueMapGroup(_QueryValueHandlerMixin, _IQueryValueStrings):
+
+    _value_maps: List[_QueryValueMap] = []
+
+    def __init__(self, query_value_maps: Iterable[_QueryValueMap] = None) -> None:
+        for value_map in query_value_maps:
+            # Type check happens in .add()
+            self.add(value_map)
+
+    def add(self, value_map: _QueryValueMap) -> None:
+        if not isinstance(value_map, _QueryValueMap):
+            raise TypeError("Object must be a _QueryValueMap")
+        self._value_maps.append(value_map)
+
+    def _get_values(self) -> Iterable[Any]:
+        result = []
+        for value_map in self._value_maps:
+            result.extend(value_map.value_args)
+        return result
+
+    @property
+    def column_str(self) -> str:
+        # Uses column str from first value_map object.
+        return self._value_maps[0].column_str if len(self._value_maps) > 0 else ""
+
+    @property
+    def values_str(self) -> str:
+        return ", ".join(map(lambda vm: vm.values_str, self._value_maps))
+
+
+class _QueryArgs(Enum):
+    VALUE = 1
+    WHERE = 2
+
+
 class _QueryBuilder(object):
 
     _table_name: str = None
-    _value_map: Dict[str, Any] = None
+    _value_map: Union[_QueryValueMap, _QueryValueMapGroup] = _QueryValueMap()
     _where_conditions: Union[Condition, ConditionSequence, ConditionGroup] = None
 
     def __init__(self,
@@ -268,26 +337,41 @@ class _QueryBuilder(object):
                  where_condition: Union[Condition, ConditionSequence, ConditionGroup] = None) -> None:
         super().__init__()
         self._table_name = table_name
-        self._value_map = value_map
+        if value_map is not None:
+            self._value_map.update(value_map)
         self._where_conditions = where_condition
 
     def _get_query_str(self) -> str:
         raise NotImplementedError()
 
-    def _get_columns_str(self):
-        return ", ".join(self._value_map.keys()) if self._value_map is not None else "*"
+    def _get_columns_str(self) -> str:
+        return self._value_map.column_str if self._value_map is not None else "*"
 
-    def _get_values_str(self):
-        return ", ".join("?"*len(self._value_map)) if self._value_map is not None else ""
+    def _get_values_str(self) -> str:
+        return self._value_map.values_str if self._value_map is not None else ""
 
-    def _get_where_str(self) -> str:
-        return 'WHERE %s' % str(self._where_conditions) if self._where_conditions is not None else ""
+    def _get_value_set_str(self) -> str:
+        return self._value_map.value_set_str if isinstance(self._value_map, _QueryValueMap) else ""
+
+    def _get_where_str(self, prefix: str = 'WHERE') -> str:
+        return ('%s %s' % (prefix, str(self._where_conditions) if self._where_conditions is not None else "")).strip()
 
     def _get_where_args(self) -> List[Any]:
-        return self._where_conditions._get_values() if self._where_conditions is not None else []
+        return self._where_conditions.value_args if self._where_conditions is not None else []
 
-    def _get_args(self) -> tuple:
-        return tuple(self._value_map.values()) if self._value_map is not None else None
+    def _get_args(self, *args: Tuple[_QueryArgs]) -> Tuple[Any]:
+        f_map = {
+            _QueryArgs.VALUE: self._value_map._get_values,
+            _QueryArgs.WHERE: self._where_conditions._get_values if self._where_conditions is not None else list
+        }
+        query_args = []
+        for arg in args:
+            if isinstance(arg, _QueryArgs):
+                args_part = f_map[arg]() if arg in f_map else None
+                if args_part is None or len(args_part) == 0:
+                    continue  # Skip
+                query_args.extend(args_part)
+        return tuple(query_args)
 
     def build(self) -> Query:
         return Query(self._get_query_str(), self._get_args()) if self._table_name is not None\
@@ -311,7 +395,7 @@ class _DropTable(_QueryBuilder):
 class InsertQuery(_QueryBuilder):
 
     def _get_query_str(self) -> str:
-        tmpl = string.Template('INSERT INTO $table ($columns) VALUES ($values)')
+        tmpl = string.Template('INSERT INTO $table $columns VALUES $values')
         return tmpl.substitute({
             "table": self._table_name,
             "columns": self._get_columns_str(),
@@ -322,40 +406,32 @@ class InsertQuery(_QueryBuilder):
 class SelectQuery(_QueryBuilder):
 
     def _get_query_str(self) -> str:
-        tmpl = string.Template('SELECT ${columns} FROM ${table} ${where}')
+        tmpl = string.Template('SELECT $columns FROM $table $where')
         return tmpl.substitute({
             "columns": self._get_columns_str(),
             "table": self._table_name,
-            "where": self._get_where_conditions() if self._where_conditions is not None else ""
+            "where": self._get_where_str()
         }).strip()
 
 
 class UpdateQuery(_QueryBuilder):
 
-    def _get_changes_string(self) -> str:
-        if self._value_map is None:
-            return ''
-        changes = []
-        for key, val in self._value_map:
-            changes.append('%s = %s' % (key, val))
-        return ', '.join(changes)
-
     def _get_query_str(self) -> str:
-        tmpl = string.Template('UPDATE ${table} SET ${changes} ${where}')
+        tmpl = string.Template('UPDATE $table SET $changes $where')
         return tmpl.substitute({
             "table": self._table_name,
-            "changes": self._get_changes_string(),
-            "where": self._get_where_conditions() if self._where_conditions is not None else ""
+            "changes": self._get_value_set_str(),
+            "where": self._get_where_str()
         }).strip()
 
 
 class DeleteQuery(_QueryBuilder):
 
     def _get_query_str(self) -> str:
-        tmpl = string.Template('DELETE FROM ${table} ${where}')
+        tmpl = string.Template('DELETE FROM $table $where')
         return tmpl.substitute({
             "table": self._table_name,
-            "where": self._get_where_conditions()
+            "where": self._get_where_str()
         }) if self._where_conditions is not None else ''
 
 
